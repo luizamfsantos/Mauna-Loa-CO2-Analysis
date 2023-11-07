b10ebd14e39c6384042b31b753df8c0741559a84
luizamfsantos <luiza.marques_@hotmail.com>
2023-11-07 13:13:15 -0300

    initial structure

---
+++ src/aggregate_model.py
@@ -0,0 +1,11 @@
+def aggregate_model(F, P, R): 
+    '''This function combines the trend, seasonality, and residual components to create the final CO2 concentration model.'''
+    pass
+
+def make_prediction(model, test_data): 
+    '''This function makes predictions using the aggregate model and plots them alongside the actual data.'''
+    pass
+
+def calculate_final_metrics(y_test, y_pred): 
+    '''This function calculates the final RMSE and MAPE for the aggregate model.'''
+    pass
