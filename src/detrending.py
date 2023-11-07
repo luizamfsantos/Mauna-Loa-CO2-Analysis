20d76e3c7e5062a1fe74caeaa2a4e9a77f8f598f
luizamfsantos <luiza.marques_@hotmail.com>
2023-11-07 13:09:58 -0300

    initial structure

---
+++ src/detrending.py
@@ -0,0 +1,19 @@
+def fit_linear_model(X_train, y_train): 
+    '''This function fits the linear model to the training data.'''
+    pass
+
+def fit_quadratic_model(X_train, y_train): 
+    '''This function fits the quadratic model to the training data.'''
+    pass
+
+def fit_cubic_model(X_train, y_train): 
+    '''This function fits the cubic model to the training data.'''
+    pass
+
+def calculate_residuals(y_test, y_pred): 
+    '''This function calculates the residual errors for a given model.'''
+    pass
+
+def calculate_metrics(y_test, y_pred): 
+    '''This function calculates the RMSE and MAPE for a given model.'''
+    pass
