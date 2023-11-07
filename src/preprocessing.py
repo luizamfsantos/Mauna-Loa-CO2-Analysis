9eeb192ef21b1911bf7b572adaa16835883992dc
luizamfsantos <luiza.marques_@hotmail.com>
2023-11-07 13:07:10 -0300

    initial structure

---
+++ src/preprocessing.py
@@ -0,0 +1,5 @@
+def preprocess_data(data):
+    '''
+    This function should handle the pre-processing steps, such as dropping data points recorded as -99.99 and splitting the data into training and test datasets.
+    '''
+    pass
