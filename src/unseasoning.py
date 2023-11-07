bc90de5a1f457e8bdbc6dff3eef820ae4630e882
luizamfsantos <luiza.marques_@hotmail.com>
2023-11-07 13:11:56 -0300

    initial structure

---
+++ src/unseasoning.py
@@ -0,0 +1,15 @@
+def remove_trend(data, model): 
+    '''This function removes the deterministic trend from the time series data based on the selected model.'''
+    pass
+
+def group_by_month(data): 
+    '''This function groups the data by month.'''
+    pass
+
+def calculate_monthly_averages(data): 
+    '''This function calculates the average signal for each month.'''
+    pass
+
+def plot_periodic_signal(data): 
+    '''This function plots the periodic signal against time.'''
+    pass
