be1049f0c01f9f03f4c413aa8ce76e3c0b95378f
luizamfsantos <luiza.marques_@hotmail.com>
2023-11-07 13:15:32 -0300

    initial structure

---
+++ src/autocovariance.py
@@ -0,0 +1,23 @@
+def remove_seasonality(data, model): 
+    '''This function removes the seasonality from the time series data based on the selected model.'''
+    pass
+
+def compute_acf_pacf(data): 
+    '''This function computes the ACF and PACF for the pre-processed data.'''
+    pass
+
+def fit_ma_model(data): 
+    '''This function fits an MA(1) model to the pre-processed data.'''
+    pass
+
+def fit_ar_model(data): 
+    '''This function fits an AR(1) model to the pre-processed data.'''
+    pass
+
+def evaluate_model_performance(model): 
+    '''This function evaluates the fitted model using AIC and BIC.'''
+    pass
+
+def conduct_residual_analysis(residuals): 
+    '''This function checks for remaining patterns in the residuals.'''
+    pass
