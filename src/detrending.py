38bc77ac0d2fe236c0542e6a464ca4eb7f917518
luizamfsantos <luiza.marques_@hotmail.com>
2023-11-09 19:45:53 -0300

    feat: calculate residuals and plot residuals

--- src/detrending.py
+++ src/detrending.py
@@ -19,9 +19,10 @@ def polynomial_regression(X, y, degree):
     --------
     tuple: A tuple containing the fitted regression model and the R-squared score.
     '''
-    poly = PolynomialFeatures(degree=degree, include_bias=False)  # create a PolynomialFeatures object
-    X_poly = poly.fit_transform(X)  # transform the features to include polynomial terms
-    reg = LinearRegression().fit(X_poly, y)  # fit the linear regression model
+    if degree > 1:
+            poly = PolynomialFeatures(degree)
+            X = poly.fit_transform(X) # transform the features to include polynomial terms
+    reg = LinearRegression().fit(X, y)  # fit the linear regression model
     coef = reg.coef_ # get the parameters of the model
     return reg, coef
 
@@ -38,15 +39,21 @@ if __name__ == '__main__':
     path = 'data/processed/CO2_clean.csv'
     df = pd.read_csv(path)
     from preprocessing import test_train_split
+    from utils import calculate_residuals, plot_residuals
     train_df, test_df = test_train_split(df)
     X_train = train_df[['t']]
     y_train = train_df[['CO2_concentration']]
+    X_test = test_df[['t']]
+    y_test = test_df[['CO2_concentration']]
     # fit a polynomial regression model of degree 1
     reg, coef = polynomial_regression(X_train, y_train, degree=1)
     print(f'y={coef[0][0]}x + {reg.intercept_[0]}')
-    # fit a polynomial regression model of degree 2
+    residuals1 = calculate_residuals(reg, X_test, y_test, degree=1)
+    # # fit a polynomial regression model of degree 2
     reg, coef = polynomial_regression(X_train, y_train, degree=2)
     print(f'y={coef[0][0]}x^2 + {coef[0][1]}x + {reg.intercept_[0]}')
+    residuals2 = calculate_residuals(reg, X_test, y_test, degree=2)
     # fit a polynomial regression model of degree 3
     reg, coef = polynomial_regression(X_train, y_train, degree=3)
     print(f'y={coef[0][0]}x^3 + {coef[0][1]}x^2 + {coef[0][2]}x + {reg.intercept_[0]}')
+    residuals3 = calculate_residuals(reg, X_test, y_test, degree=3)
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,53 @@
 from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.preprocessing import PolynomialFeatures
+
+def calculate_residuals(model, X_test, y_test, degree):
+    '''
+    Calculate the residuals for a given model.
+
+    Parameters:
+    model (object): The trained regression model.
+    X_test (array-like): Test data features.
+    y_test (array-like): True labels for the test data.
+    degree (int): The degree of the polynomial regression model.
+
+    Returns:
+    residuals (array-like): Residual errors calculated as y_test - model.predict(X_test).
+    '''
+    try:
+        # Transform X to the correct degree if degree > 1
+        if degree > 1:
+            poly = PolynomialFeatures(degree)
+            X_test = poly.fit_transform(X_test)
+        
+        # Predict with the model
+        y_pred = model.predict(X_test)
+        
+        # Calculate residuals
+        residuals = y_test - y_pred
+
+        return residuals
+    except Exception as e:
+        raise ValueError(f"An error occurred during residual calculation: {e}")
+        
+def plot_residuals(residuals):
+    '''
+    Plot the residuals for a given model.
+
+    Parameters:
+    residuals (array-like): Residual errors.
+
+    Returns:
+    None
+    '''
+    plt.scatter(np.arange(len(residuals)), residuals, color='b', alpha=0.6)
+    plt.title('Residuals Plot')
+    plt.xlabel('Observation')
+    plt.ylabel('Residuals')
+    plt.show()
+
 
 def estimate_parameters(X_train, y_train, degree):
     # Estimate parameters for the polynomial model
